e21e1c65a612f0cd2128a7e27ea29b0807efe63c
vanessaborges2 <vanessa.borges2@fatec.sp.gov.br>
2024-08-31 09:39:25 -0300

    Exercício 1

---
+++ lista3/app/Http/Controllers/ListaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListaController extends Controller
+{
+    public function mostrarExer1(){
+        return view("exer1");
+    }
+
+    public function calcularExer1(Request $request){
+        $valor1 = (int)$request->input('valor1');
+        $valor2 = (int)$request->input('valor2');
+        return $valor1+$valor2;
+    }
+}
---
+++ lista3/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListaController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/exer1', [ListaController::class, 'mostrarExer1']);
+Route::post('/respostaexer1', [ListaController::class, 'calcularExer1']);
